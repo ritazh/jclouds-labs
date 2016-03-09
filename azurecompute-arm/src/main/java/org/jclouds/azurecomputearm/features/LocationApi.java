3d5033232428bf0929da920571d54acdb4f62602
Janne Koskinen <jtjk@iki.fi>
2016-03-09 13:52:38 +0200

    Add LocationApi

--- azurecompute-arm/src/main/java/org/jclouds/azurecomputearm/features/LocationApi.java
+++ azurecompute-arm/src/main/java/org/jclouds/azurecomputearm/features/LocationApi.java
@@ -22,14 +22,17 @@ import javax.inject.Named;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.jclouds.Fallbacks.EmptyListOnNotFoundOr404;
 import org.jclouds.azurecomputearm.domain.Location;
-import org.jclouds.azurecomputearm.xml.ListLocationsHandler;
-import org.jclouds.rest.annotations.Fallback;
+import org.jclouds.azurecomputearm.oauth.v2.filters.OAuthFilter;
 import org.jclouds.rest.annotations.Headers;
-import org.jclouds.rest.annotations.XMLResponseParser;
+import org.jclouds.rest.annotations.QueryParams;
+import org.jclouds.rest.annotations.RequestFilters;
+import org.jclouds.rest.annotations.SelectJson;
+import org.jclouds.rest.annotations.Fallback;
 
 /**
  * The Service Management API includes operations for listing the available data center locations for a cloud service in
@@ -38,9 +41,12 @@ import org.jclouds.rest.annotations.XMLResponseParser;
  *
  * @see <a href="http://msdn.microsoft.com/en-us/library/gg441299">docs</a>
  */
-@Path("/locations")
+@Path("/subscriptions/{subscriptionid}")
 @Headers(keys = "x-ms-version", values = "{jclouds.api-version}")
-@Consumes(MediaType.APPLICATION_XML)
+@RequestFilters(OAuthFilter.class)
+@QueryParams(keys = "api-version", values = "2015-11-01")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public interface LocationApi {
 
    /**
@@ -48,7 +54,8 @@ public interface LocationApi {
     */
    @Named("ListLocations")
    @GET
-   @XMLResponseParser(ListLocationsHandler.class)
+   @Path("/locations")
+   @SelectJson("value")
    @Fallback(EmptyListOnNotFoundOr404.class)
    List<Location> list();
 }
